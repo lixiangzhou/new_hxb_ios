29642ca38f7421c2ef7148c8e58e47a3b4edf001
李向洲 <lixiangzhouno1@163.com>
2018-06-29 16:02:45 +0800

    HXBNetActivityManager OK

--- HXB/HXB/Classes/Network/HXBNetActivityManager.swift
+++ HXB/HXB/Classes/Network/HXBNetActivityManager.swift
@@ -7,7 +7,43 @@
 //
 
 import UIKit
+import ReactiveSwift
+import ReactiveCocoa
 
-class HXBNetActivityManager: NSObject {
-
+class HXBNetActivityManager {
+    static let shared = HXBNetActivityManager()
+    
+    private let requestCount = MutableProperty(0)
+    private var lock = NSLock()
+    
+    private init() {
+        UIApplication.shared.reactive.makeBindingTarget { $0.isNetworkActivityIndicatorVisible = $1 } <~ requestCount.signal.map { $0 > 0 }
+    }
+    
+    func sendRequest() {
+        safeCount {
+            requestCount.value += 1
+        }
+    }
+    
+    func finishRequest() {
+        safeCount {
+            requestCount.value -= 1
+        }
+    }
+    
+    private func safeCount(_ count: () -> Void) {
+        lock.lock()
+        count()
+        lock.unlock()
+    }
+    
+    static func sendRequest() {
+        HXBNetActivityManager.shared.sendRequest()
+    }
+    
+    static func finishRequest() {
+        HXBNetActivityManager.shared.finishRequest()
+    }
+    
 }
